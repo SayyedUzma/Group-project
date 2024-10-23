e4dd60834b4511ce79c2db955f0c87665d324d3e
Janhavipreet <137142960+Janhavipreet@users.noreply.github.com>
2024-10-23 19:47:19 +0530

    Add files via upload
    Login pages code

---
+++ script.js
@@ -0,0 +1,6 @@
+const logBox = document.querySelector('.log-box');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click',() => { logBox.classList.add('active');});
+loginLink.addEventListener('click',() => { logBox.classList.remove('active');});
